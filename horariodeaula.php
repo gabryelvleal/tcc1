dcc52304543a5419bae2a5fcb14dc2bcc331ba4c
Gabryel Vitor Leal <101608705+gabryelvleal@users.noreply.github.com>
2022-08-06 19:08:57 -0300

    Add files via upload

---
+++ horariodeaula.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+table, th, td {
+border: 1px solid black;
+border-collapse: collapse;
+}
+    </style>
+</head>
+<body>
+    <h1>Horário Escolar</h1>
+    <br>
+    <table>
+<tr>
+<th>Horário</th>
+<th colspan="5">Dia</th>
+</tr>
+
+<tr>
+<td>    </td>
+<th>Segunda</th>
+<th>Terça</th>
+<th>Quarta</th>
+<th>Quinta</th>
+<th>Sexta</th>
+</tr>
+
+<tr>
+<td>07:30</td>
+</tr>
+
+<tr>
+<td>08:20</td>
+</tr>
+
+<tr>
+<td>09:10</td>
+</tr>
+
+<tr> 
+<th colspan="6">Intervalo</th>
+</tr>
+
+<tr>
+<td> 09:30</td>
+</tr>
+
+<tr>
+<td>10:20</td>
+</tr>  
+
+<tr>
+<td>11:10</td>
+</tr>
+
+<tr>
+<th colspan="6">Almoço</th>
+</tr>
+
+<tr>
+<td>13:20</td>
+</tr>
+
+<tr>
+<td>14:10</td>
+</tr>
+
+<tr>
+ <th colspan="6">Intervalo</th>
+</tr>
+
+<tr>
+<td>15:20</td>
+</tr>
+
+<tr>
+<td>16:10</td>
+</tr>
+
+<tr>
+<th colspan="6">Saída</th>
+</tr>
+
+</table>
+</body>
+</html>
